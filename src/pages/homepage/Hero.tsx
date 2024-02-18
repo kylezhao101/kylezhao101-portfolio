a778da570dacc7f5fdd03c3cb4fac54a2e8c4b51
kylezhao101 <kylezhao101@gmail.com>
2024-02-17 16:28:50 -0800

    Hotfix hero spacing

--- src/pages/homepage/Hero.tsx
+++ src/pages/homepage/Hero.tsx
@@ -25,7 +25,7 @@ const Hero: React.FC = () => {
 
   return (
     <section className="flex h-screen flex-col justify-center bg-custom-background-gray px-4 sm:px-20">
-      <div className="pb-20 pt-20 sm:pt-0">
+      <div className="pb-20 pt-44 sm:pt-0">
         <p className="font-space">Hello, I'm</p>
         <h1 className="font-neopixel text-8xl md:text-9xl">Kyle Zhao</h1>
       </div>
