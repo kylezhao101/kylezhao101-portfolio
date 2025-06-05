ec8bec258660d541c74b44b2b0b7e4c15741f798
Kyle <kylezhao101@gmail.com>
2025-06-04 21:26:47 -0700

    fix external link navigation

--- src/components/ProjectNav.tsx
+++ src/components/ProjectNav.tsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom";
 
-const ProjectNav: React.FC<{ projectsData: Array<any> }> = ({
+interface Project {
+  title: string;
+  type: string;
+  date?: string;
+  viewMoreLink?: string;
+  apiReferenceLink?: string;
+}
+
+const ProjectNav: React.FC<{ projectsData: Project[] }> = ({
   projectsData,
 }) => {
   return (
@@ -10,23 +18,31 @@ const ProjectNav: React.FC<{ projectsData: Array<any> }> = ({
         <h3 className="mr-5 font-space text-xl">Project Directory</h3>
       </div>
 
-      {projectsData.map((project, index) => (
-        <Link
-          to={
-            project.apiReferenceLink
-              ? project.apiReferenceLink
-              : project.viewMoreLink
-          }
-          target={project.apiReferenceLink ? "_blank" : "_self"}
-          rel={project.apiReferenceLink ? "noopener noreferrer" : ""}
-          key={index}
-          className="grid grid-cols-12 border-t border-black py-2 transition duration-200 ease-in-out hover:text-custom-blue"
-        >
-          <p className="col-span-2 hidden pr-2 md:block">{project.date}</p>
-          <p className="col-span-6 pr-2 md:col-span-4">{project.title}</p>
-          <p className="col-span-6 pr-2">{project.type}</p>
-        </Link>
-      ))}
+      {projectsData.map((project, index) =>
+        project.apiReferenceLink ? (
+          <a
+            href={project.apiReferenceLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={index}
+            className="grid grid-cols-12 border-t border-black py-2 transition duration-200 ease-in-out hover:text-custom-blue"
+          >
+            <p className="col-span-2 hidden pr-2 md:block">{project.date}</p>
+            <p className="col-span-6 pr-2 md:col-span-4">{project.title}</p>
+            <p className="col-span-6 pr-2">{project.type}</p>
+          </a>
+        ) : (
+          <Link
+            to={project.viewMoreLink!}
+            key={index}
+            className="grid grid-cols-12 border-t border-black py-2 transition duration-200 ease-in-out hover:text-custom-blue"
+          >
+            <p className="col-span-2 hidden pr-2 md:block">{project.date}</p>
+            <p className="col-span-6 pr-2 md:col-span-4">{project.title}</p>
+            <p className="col-span-6 pr-2">{project.type}</p>
+          </Link>
+        ),
+      )}
     </div>
   );
 };
