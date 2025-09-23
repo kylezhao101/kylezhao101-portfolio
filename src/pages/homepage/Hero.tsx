7baecf7e2c326572432d185328628671fe5aaa2c
kylezhao101 <kylezhao101@gmail.com>
2025-09-23 12:44:38 -0700

    fix swipte

--- src/pages/homepage/Hero.tsx
+++ src/pages/homepage/Hero.tsx
@@ -13,9 +13,11 @@ const Hero: React.FC = () => {
       <div className="grid grid-cols-4 gap-5 sm:grid-cols-8 sm:gap-0 md:grid-cols-8 lg:grid-cols-12">
         <div className="col-span-4 flex items-start sm:col-span-3">
           <div className="mr-3 mt-2 h-3 w-3 bg-custom-blue"></div>
-          <h3 className="swipe-text mr-5 font-space text-xl">
-            Designer + Developer
-          </h3>
+          <div className="swipe">
+            <h3 className="swipe-text mr-5 font-space text-xl">
+              Designer + Developer
+            </h3>
+          </div>
         </div>
 
         <div className="swipe col-span-4 col-start-1 mr-5 font-space sm:col-span-4 md:col-span-4 md:col-start-5 lg:col-span-3 lg:col-start-4">
